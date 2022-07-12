b7eb30d1dc2c714f75aa582baa607b18dcd30bd7
Gio <you@example.com>
2022-07-12 17:00:45 +0800

    set up complete

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
-console.log("hey");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/status", (req, res) => {
+  res.send({
+    message: "Hello world",
+  });
+});
+
+app.listen(8082, (err) => {
+  if (err) throw error;
+  console.log("Server is listening at 8082");
+});
